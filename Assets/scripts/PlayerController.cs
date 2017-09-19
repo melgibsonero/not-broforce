4c27a38fdf00eeebe5e703464996cdf9ec572eb1
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2017-09-19 19:08:14 +0300

    Added utility scripts which may or may not be useful

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,116 +2,121 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace not_broforce
 {
-    private Animator animator;
-    private SpriteRenderer sr;
-    private Rigidbody2D rb;
-    
-    private bool CanJump = true;
-    private bool left = false;
-    private float xoffset = 0.225f;
-    private float distance;
-    
-
-    public GameObject box;
-    public float speed;
-    public float thrust;
-    public float boxDistance;
-    public float minDistance;
-    public float boxSpeed;
-    // Use this for initialization
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
+        private Animator animator;
+        private SpriteRenderer sr;
+        private Rigidbody2D rb;
 
-        box = transform.GetChild(0).gameObject;
-    }
+        private bool CanJump = true;
+        private bool left = false;
+        private float xoffset = 0.225f;
+        private float distance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-        if (left)
+
+        public GameObject box;
+        public float speed;
+        public float thrust;
+        public float boxDistance;
+        public float minDistance;
+        public float boxSpeed;
+
+        // Use this for initialization
+        void Start()
         {
-            sr.flipX = true;
-        }
-        else
-        {
-            sr.flipX = false;
-        }
-        if (transform.childCount > 0)
-        {
-            MoveBoxes();
-        }
-    }
+            animator = GetComponent<Animator>();
+            sr = GetComponent<SpriteRenderer>();
+            rb = GetComponent<Rigidbody2D>();
 
-    void FixedUpdate()
-    {
-        
-    }
-
-    void Move()
-    {
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1) {
-            float horizontal = Input.GetAxis("Horizontal") * speed;
-            if (horizontal < 0)
-            {
-            left = true;
-            }
-            else
-            {
-            left = false;
-            }
-            transform.Translate(horizontal, 0, 0);
+            box = transform.GetChild(0).gameObject;
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.W))
+        // Update is called once per frame
+        void Update()
         {
-            rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
-        } 
-        if (Input.GetKey(KeyCode.F))
-        {
-            box.GetComponent<Rigidbody2D>().simulated = false;
-            box.GetComponent<BoxCollider2D>().enabled = false;
+            Move();
             if (left)
             {
-                xoffset = -0.225f;
+                sr.flipX = true;
             }
             else
             {
-                xoffset = 0.225f;
+                sr.flipX = false;
             }
-            box.transform.position = new Vector3(transform.position.x + xoffset, transform.position.y + 0.025f, 0);
-            animator.SetBool("setcrate", true);
-        }
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            Debug.Log("non-clickety F");
-            box.GetComponent<Rigidbody2D>().simulated = true;
-            box.GetComponent<BoxCollider2D>().enabled = true;
-            box.GetComponent<SpriteRenderer>().color = Color.white;
-            box.transform.parent = null;
-            animator.SetBool("setcrate", false);
-        }
-    }
-    void MoveBoxes()
-    {
-        distance = Vector3.Distance(gameObject.transform.position, transform.GetChild(0).gameObject.transform.position);
-        Debug.Log(distance);
-        Vector3 newPosition = gameObject.transform.position; //basically same as not having it but saved for later use.
-        float T = Time.deltaTime * distance * minDistance * boxSpeed;
-
-        if (T > 0.2f)
-        {
-            T = 0.2f;
+            if (transform.childCount > 0)
+            {
+                MoveBoxes();
+            }
         }
 
-        if (distance > boxDistance)
+        void FixedUpdate()
         {
-            transform.GetChild(0).gameObject.transform.position = Vector3.Lerp(box.transform.position, newPosition, T);
+
+        }
+
+        void Move()
+        {
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1)
+            {
+                float horizontal = Input.GetAxis("Horizontal") * speed;
+                if (horizontal < 0)
+                {
+                    left = true;
+                }
+                else
+                {
+                    left = false;
+                }
+                transform.Translate(horizontal, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.W))
+            {
+                rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                box.GetComponent<Rigidbody2D>().simulated = false;
+                box.GetComponent<BoxCollider2D>().enabled = false;
+                if (left)
+                {
+                    xoffset = -0.225f;
+                }
+                else
+                {
+                    xoffset = 0.225f;
+                }
+                box.transform.position = new Vector3(transform.position.x + xoffset, transform.position.y + 0.025f, 0);
+                animator.SetBool("setcrate", true);
+            }
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                //Debug.Log("non-clickety F");
+                box.GetComponent<Rigidbody2D>().simulated = true;
+                box.GetComponent<BoxCollider2D>().enabled = true;
+                box.GetComponent<SpriteRenderer>().color = Color.white;
+                box.transform.parent = null;
+                animator.SetBool("setcrate", false);
+            }
+        }
+        void MoveBoxes()
+        {
+            distance = Vector3.Distance(gameObject.transform.position, transform.GetChild(0).gameObject.transform.position);
+            Debug.Log(distance);
+            Vector3 newPosition = gameObject.transform.position; //basically same as not having it but saved for later use.
+            float T = Time.deltaTime * distance * minDistance * boxSpeed;
+
+            if (T > 0.2f)
+            {
+                T = 0.2f;
+            }
+
+            if (distance > boxDistance)
+            {
+                transform.GetChild(0).gameObject.transform.position = Vector3.Lerp(box.transform.position, newPosition, T);
+            }
         }
     }
 }
---
+++ Assets/scripts/Utils.cs
@@ -0,0 +1,441 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace not_broforce
+{
+    class Utils
+    {
+        /// <summary>
+        /// Checks if the given character is a number.
+        /// </summary>
+        /// <param name="c">a character</param>
+        /// <returns>is the given character a number</returns>
+        public static bool IsNumber(char c)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //public static bool CheckIfSameType(Type type1, Type type2)
+        //{
+        //    return (type1 == type2);
+        //}
+
+        //public static bool CheckIfSameTypeOrSubtype(Type subtype, Type type)
+        //{
+        //    return (subtype.IsSubclassOf(type) ||
+        //            CheckIfSameType(subtype, type));
+        //}
+
+
+        public static int WithinLimits(int number, int minLimit, int maxLimit)
+        {
+            if (minLimit > maxLimit)
+            {
+                int temp = minLimit;
+                minLimit = maxLimit;
+                maxLimit = temp;
+            }
+
+            if (number < minLimit)
+            {
+                return minLimit;
+            }
+            else if (number > maxLimit)
+            {
+                return maxLimit;
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+        ///// <summary>
+        ///// Creates and returns a rectangle based on two elements' positions.
+        ///// </summary>
+        ///// <param name="topLeftElementPos">the position of an element at
+        ///// the top left corner of the area</param>
+        ///// <param name="bottomRightElementPos">the position of an element at
+        ///// the bottom right corner of the area</param>
+        ///// <returns></returns>
+        //public static Rectangle ElementArea(Vector2 topLeftElementPos, Vector2 bottomRightElementPos)
+        //{
+        //    return (new Rectangle((int) topLeftElementPos.X, (int) topLeftElementPos.Y,
+        //                          (int) (bottomRightElementPos.X + Level.ElementSideLength - topLeftElementPos.X),
+        //                          (int) (bottomRightElementPos.Y + Level.ElementSideLength - topLeftElementPos.Y)));
+        //}
+
+        /// <summary>
+        /// Calculates the amount of damage dealt.
+        /// Damage is not based on attack strength but how many times
+        /// can the game character or level element be attacked until it
+        /// is destroyed (example: lethal floor kills the character with 2 hits
+        /// from full health regardless of the max HP).
+        /// </summary>
+        /// <param name="maxHitpoints">the victim's maximum hitpoints</param>
+        /// <param name="hitsToKill">the attacker's power</param>
+        /// <returns>the amount of damage dealt</returns>
+        public static int Damage(int maxHitpoints, int hitsToKill)
+        {
+            if (hitsToKill == 1)
+            {
+                return maxHitpoints;
+            }
+            else if (hitsToKill > 1)
+            {
+                int damage = maxHitpoints / hitsToKill;
+
+                // Integer rounding error correction
+                while (damage * hitsToKill < maxHitpoints)
+                {
+                    damage++;
+                }
+
+                return damage;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //public static void CheckExplosionDamage(PlayerCharacter character, List<Explosion> explosions)
+        //{
+        //    if (!character.Dead)
+        //    {
+        //        foreach (Explosion explosion in explosions)
+        //        {
+        //            if (explosion.lethal &&
+        //                (Nav.WithinArea(character.position, explosion.position, explosion.size) ||
+        //                 Nav.WithinArea(character.targetPosition, explosion.position, explosion.size)))
+        //            {
+        //                character.TakeDamage(1);
+        //                return;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static void CheckExplosionDamage(Element element, List<Explosion> explosions)
+        //{
+        //    if (element.IsFunctional)
+        //    {
+        //        foreach (Explosion explosion in explosions)
+        //        {
+        //            if (explosion.lethal &&
+        //                (Nav.WithinArea(element.position, explosion.position, explosion.size) ||
+        //                 Nav.WithinArea(element.targetPosition, explosion.position, explosion.size)))
+        //            {
+        //                element.TakeDamage(1, true);
+        //            }
+        //        }
+        //    }
+        //}
+
+        public static Vector2 GetScaleFromSize(float actualWidth, float actualHeight, float newWidth, float newHeight)
+        {
+            int width = (int) (newWidth / actualHeight);
+            int height = (int) (newHeight / actualHeight);
+
+            return new Vector2(width, height);
+        }
+
+        ///// <summary>
+        ///// Splits the given text into lines and returns them.
+        ///// </summary>
+        ///// <param name="font">a font</param>
+        ///// <param name="text">a text</param>
+        ///// <param name="maxLength">a line's maximum length</param>
+        ///// <returns>lines of text</returns>
+        //public static List<string> SplitTextIntoLines(SpriteFont font, string text, float maxLength)
+        //{
+        //    List<string> lines = new List<string>();
+
+        //    if (text.Length > 0)
+        //    {
+        //        string line = "";
+
+        //        for (int i = 0; i < text.Length; i++)
+        //        {
+        //            // If a semicolon is found, a new line is started
+        //            if (text[i] == ';')
+        //            {
+        //                lines.Add(line);
+
+        //                line = "";
+        //            }
+        //            // If the line has become too long, a new line is started
+        //            else if (font.MeasureString(line).X > maxLength)
+        //            {
+        //                line += text[i];
+
+        //                lines.Add(line);
+
+        //                line = "";
+        //            }
+        //            // If the text ends, the last line is added to the list
+        //            else if (i == text.Length - 1)
+        //            {
+        //                line += text[i];
+
+        //                lines.Add(line);
+
+        //                line = "";
+        //            }
+        //            // Otherwise the character is added to the line
+        //            else
+        //            {
+        //                line += text[i];
+        //            }
+        //        }
+        //    }
+
+        //    // Returns the lines
+        //    return lines;
+        //}
+
+        /// <summary>
+        /// Splits the given text into lines and returns them.
+        /// Splits the text at each semicolon in it.
+        /// </summary>
+        /// <param name="text">a text</param>
+        /// <returns>lines of text</returns>
+        public static List<string> SplitTextIntoLines(string text)
+        {
+            List<string> lines = new List<string>();
+
+            if (text.Length > 0)
+            {
+                string line = "";
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    // If a semicolon is found, a new line is started
+                    if (text[i] == ';')
+                    {
+                        lines.Add(line);
+
+                        line = "";
+                    }
+                    // If the text ends, the last line is added to the list
+                    else if (i == text.Length - 1)
+                    {
+                        line += text[i];
+
+                        lines.Add(line);
+
+                        line = "";
+                    }
+                    // Otherwise the character is added to the line
+                    else
+                    {
+                        line += text[i];
+                    }
+                }
+            }
+
+            // Returns the lines
+            return lines;
+        }
+
+        public static void MergeStringLists(List<string> lines1, List<string> lines2)
+        {
+            foreach (string line in lines2)
+            {
+                lines1.Add(line);
+            }
+        }
+
+        public static string RomanNumerals(int number)
+        {
+            // I V X L C D M
+
+            string romanNumerals = "";
+
+            if (number > 1000)
+            {
+                number = 0;
+                romanNumerals = "M+";
+            }
+            else if (number == 1000)
+            {
+                number = 0;
+                romanNumerals += "M";
+            }
+            else if (number >= 1000 - 200)
+            {
+                romanNumerals += RomanNumerals2(number, 1000);
+                number -= 800;
+                if (number >= 100)
+                {
+                    number -= 100;
+                }
+            }
+            else if (number >= 500)
+            {
+                number -= 500;
+                romanNumerals += "D";
+            }
+            else if (number >= 500 - 200)
+            {
+                romanNumerals += RomanNumerals2(number, 500);
+                number -= 300;
+                if (number >= 100)
+                {
+                    number -= 100;
+                }
+            }
+            else if (number >= 100)
+            {
+                number -= 100;
+                romanNumerals += "C";
+            }
+            else if (number >= 100 - 20)
+            {
+                romanNumerals += RomanNumerals2(number, 100);
+                number -= 80;
+                if (number >= 10)
+                {
+                    number -= 10;
+                }
+            }
+            else if (number >= 50)
+            {
+                number -= 50;
+                romanNumerals += "L";
+            }
+            else if (number >= 50 - 20)
+            {
+                romanNumerals += RomanNumerals2(number, 50);
+                number -= 30;
+                if (number >= 10)
+                {
+                    number -= 10;
+                }
+            }
+            else if (number >= 10)
+            {
+                number -= 10;
+                romanNumerals += "X";
+            }
+            else if (number >= 10 - 2)
+            {
+                romanNumerals += RomanNumerals2(number, 10);
+                number -= 8;
+                if (number >= 1)
+                {
+                    number -= 1;
+                }
+            }
+            else if (number >= 5)
+            {
+                number -= 5;
+                romanNumerals += "V";
+            }
+            else if (number == 4)
+            {
+                number = 0;
+                romanNumerals += "IV";
+            }
+            else if (number >= 1)
+            {
+                number -= 1;
+                romanNumerals += "I";
+            }
+
+            if (number > 0)
+            {
+                romanNumerals += RomanNumerals(number);
+                return romanNumerals;
+            }
+            else
+            {
+                return romanNumerals;
+            }
+        }
+
+        public static string RomanNumerals2(int number, int almostNumber)
+        {
+            // I V X L C D M
+
+            string romanNumerals = "";
+
+            if (almostNumber == 1000)
+            {
+                if (number >= 900)
+                {
+                    romanNumerals += "CM";
+                }
+                else if (number >= 800)
+                {
+                    romanNumerals += "DCCC";
+                }
+            }
+            else if (almostNumber == 500)
+            {
+                if (number >= 400)
+                {
+                    romanNumerals += "CD";
+                }
+                else if (number >= 300)
+                {
+                    romanNumerals += "CCC";
+                }
+            }
+            else if (almostNumber == 100)
+            {
+                if (number >= 90)
+                {
+                    romanNumerals += "XC";
+                }
+                else if (number >= 80)
+                {
+                    romanNumerals += "LXXX";
+                }
+            }
+            else if (almostNumber == 50)
+            {
+                if (number >= 40)
+                {
+                    romanNumerals += "XL";
+                }
+                else if (number >= 30)
+                {
+                    romanNumerals += "XXX";
+                }
+            }
+            else if (almostNumber == 10)
+            {
+                if (number == 9)
+                {
+                    romanNumerals += "IX";
+                }
+                else if (number == 8)
+                {
+                    romanNumerals += "VIII";
+                }
+            }
+            else if (almostNumber == 5)
+            {
+                if (number == 4)
+                {
+                    romanNumerals += "IV";
+                }
+                else if (number == 3)
+                {
+                    romanNumerals += "III";
+                }
+            }
+
+            return romanNumerals;
+        }
+    }
+}
