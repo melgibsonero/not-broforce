018de3a1efe1c31181476ff05eecc00f15af28f0
WPK\c5mlouhi <mlouhio@hotmail.com>
2017-09-25 12:16:06 +0300

    DID SUM SHIT. jupming nda momevent

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -22,6 +22,7 @@ public class PlayerController : MonoBehaviour
 
     public GameObject box;
     public float speed;
+    public float maxSpeed;
     public float thrust;
     public float wallthrust;
     public float boxDistance;
@@ -80,6 +81,7 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
+        Debug.Log(rb.velocity);
         if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1)
         {
             float horizontal = Input.GetAxis("Horizontal") * speed;
@@ -94,27 +96,44 @@ public class PlayerController : MonoBehaviour
                     left = false;
                 }
             }
-            transform.Translate(horizontal, 0, 0);
-            //rb.AddForce(new Vector2(horizontal,0), ForceMode2D.Force);
+            //transform.Translate(horizontal, 0, 0);
+            rb.AddForce(new Vector2(horizontal,0), ForceMode2D.Force);
+            if (rb.velocity.x > maxSpeed)
+            {
+                rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+            }
+            if(rb.velocity.x < -maxSpeed)
+            {
+                rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+            }
+            
+        }
+        else
+        {
+            //zeroing movement speed to 0
+            if (Grounded)
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.W))
         {
             if (Grounded)
             {
-                rb.velocity = Vector2.zero;
+                rb.velocity = new Vector2(rb.velocity.x, 0);
                 rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
                 Grounded = false;
             }
             if (Walled)
             {
-                rb.velocity = Vector2.zero;
+                rb.velocity = new Vector2(rb.velocity.x, 0);
                 rb.AddForce(walljumpdir, ForceMode2D.Impulse);
                 left = left == true ? false : true;
                 Walled = false;
             }
         }
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.Joystick1Button2) || Input.GetKey(KeyCode.F))
         {
             if (CanBox)
             {
@@ -134,7 +153,7 @@ public class PlayerController : MonoBehaviour
             box.transform.position = new Vector3(transform.position.x + xoffset, transform.position.y + 0.025f, 0);
             animator.SetBool("setcrate", true);
         }
-        if (Input.GetKeyUp(KeyCode.F))
+        if (Input.GetKeyUp(KeyCode.Joystick1Button2) || Input.GetKeyUp(KeyCode.F))
         {
             Debug.Log("non-clickety F");
             box.GetComponent<Rigidbody2D>().simulated = true;
