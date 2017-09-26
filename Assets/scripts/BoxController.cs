7019a7a5c2d5dc70f55a20efccf00890beec4c1a
Valtteri Ojanen <valtteri.ojanen@cs.tamk.fi>
2017-09-26 12:34:06 +0300

    Implemented movement for Boxes

--- Assets/scripts/BoxController.cs
+++ Assets/scripts/BoxController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxController : MonoBehaviour {
+namespace not_broforce {
+    public class BoxController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [SerializeField]
+        private List<BoxMovement> boxes = new List<BoxMovement>();
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void addBox (BoxMovement box) {
+            boxes.Add(box);
+            RefreshFollowTargets();
+        }
+
+        private void RefreshFollowTargets() {
+            for(int i = 0; i < boxes.Count; i++) {
+                if(i == 0) {
+                    boxes[i].AddFollowTarget(GameObject.FindGameObjectWithTag("Player").transform);
+                } else {
+                    boxes[i].AddFollowTarget(boxes[i - 1].transform);
+                } 
+            }
+        }
+    }
 }
---
+++ Assets/scripts/BoxMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace not_broforce {
+    public class BoxMovement : MonoBehaviour {
+
+        [SerializeField]
+        private Transform _target;
+
+        [SerializeField]
+        private float _speed;
+
+        [SerializeField]
+        private float _followDistance;
+
+        private Rigidbody2D _RB;
+
+        [SerializeField]
+        private float distanceX;
+
+        [SerializeField]
+        private float distanceY;
+
+        [SerializeField]
+        private float _thrust;
+
+        private int mask = (1 << 8);
+
+        private Vector2 _moveDirection;
+
+        void Start() {
+            GameObject.FindGameObjectWithTag("crate").GetComponent<BoxController>().addBox(this);
+            _RB = gameObject.GetComponent<Rigidbody2D>();
+            
+            mask = ~mask;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            Move();
+        }
+
+        public void AddFollowTarget (Transform target) {
+            this._target = target;
+        }
+
+        private void Move() {
+            if(_target != null) {
+                if (Vector3.Distance(transform.position,
+                _target.position) > _followDistance) {
+                    Physics2D.queriesStartInColliders = false;
+                    
+
+                   
+                    Vector3 direction = new Vector3 (_target.position.x - transform.position.x,0,0).normalized;
+                    if(direction.x > 0) {
+                        _moveDirection = Vector2.right;
+                        
+                    } else {
+                        _moveDirection = Vector2.left;
+                    }
+                    int mask = (1 << 8);
+                    mask = ~mask;
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, _moveDirection, distanceX,mask);
+                    if (hit.collider != null) {
+                        
+                        Jump();
+                    }
+                    transform.Translate(direction * _speed * Time.deltaTime);
+                }
+            }
+        }
+
+        private void Jump () {
+            Physics2D.queriesStartInColliders = false;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanceY,mask);
+            bool Grounded = false;
+            if (hit.collider != null) {
+                Debug.Log("jumping");
+                Grounded = true;
+            }
+            if (Grounded) {
+                Debug.Log("jumping");
+                _RB.AddForce(transform.up * _thrust, ForceMode2D.Impulse);
+                
+            }
+        }
+
+    }
+}
