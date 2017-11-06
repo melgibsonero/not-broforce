67fec77ab362a5b3120b686b09454404f8dd356b
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2017-11-06 10:18:15 +0200

    Implemented placeholder main menu

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace not_broforce
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private Scene scene;
+
+        private enum Scene
+        {
+            TestZoneX,
+            ValtterinPlayground,
+            KatsoaSaaMutteiKoskela
+        }
+
+        private string SceneName(Scene scene)
+        {
+            // TODO: Better way to change scene
+
+            string sceneName = "";
+
+            switch (scene)
+            {
+                case Scene.TestZoneX:
+                {
+                    sceneName = "TestZoneX";
+                    break;
+                }
+                case Scene.ValtterinPlayground:
+                {
+                    sceneName = "Valtterin Playground";
+                    break;
+                }
+                case Scene.KatsoaSaaMutteiKoskela:
+                {
+                    sceneName = "Katsoa saa muttei Koskela";
+                    break;
+                }
+            }
+
+            return sceneName;
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(SceneName(scene));
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
